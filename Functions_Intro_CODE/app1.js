c81f031d19559b37df2a14ced41caa39feb6914c
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-11-06 22:57:23 -0500

    done with functions

---
+++ Functions_Intro_CODE/app1.js
@@ -0,0 +1,54 @@
+// function singSong() {
+//   console.log("first function");
+// }
+
+// singSong();
+
+// function singSong(name) {
+//   //here name is parameter
+//   console.log("first function");
+//   console.log(`song name is ${name}`);
+// }
+// singSong("baby"); //here baby is argument
+
+// function isSnakeEyes(a, b) {
+//   if (a === 1 && b === 1) {
+//     console.log("Snake Eyes!");
+//   } else {
+//     console.log("Not Snake Eyes!");
+//   }
+// }
+// isSnakeEyes(1, 2);
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+// multiply(1, 2);
+
+// function isShortWeather(num) {
+//   if (num >= 75) {
+//     return true;
+//   }
+//   return false;
+// }
+// isShortWeather(80);
+
+// function lastElement(arr) {
+//   if (arr.length === 0) {
+//     return null;
+//   } else {
+//     return arr[arr.length-1];
+//   }
+// }
+// lastElement([3, 5, 6]);
+
+
+// function capitalize(a){
+    
+//      return (a.charAt(0).toUpperCase()).concat(a.slice(1));
+// }
+
+
+
+
+
