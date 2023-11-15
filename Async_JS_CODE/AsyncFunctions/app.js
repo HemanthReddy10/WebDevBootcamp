8b2132c1c1006b78f0c0305a9a4a8c928fb3f934
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-11-15 00:07:17 -0500

    changes

--- Async_JS_CODE/AsyncFunctions/app.js
+++ Async_JS_CODE/AsyncFunctions/app.js
@@ -1,72 +1,72 @@
 // async function hello() {
 // }
 
-const sing = async () => {
-  throw "OH NO, PROBLEM!";
-  return "LA LA LA LA";
-};
+// const sing = async () => {
+//   throw "OH NO, PROBLEM!";
+//   return "LA LA LA LA";
+// };
 
-sing()
-  .then((data) => {
-    console.log("PROMISE RESOLVED WITH:", data);
-  })
-  .catch((err) => {
-    console.log("OH NO, PROMISE REJECTED!");
-    console.log(err);
-  });
+// sing()
+//   .then((data) => {
+//     console.log("PROMISE RESOLVED WITH:", data);
+//   })
+//   .catch((err) => {
+//     console.log("OH NO, PROMISE REJECTED!");
+//     console.log(err);
+//   });
 
-const login = async (username, password) => {
-  if (!username || !password) throw "Missing Credentials";
-  if (password === "corgifeetarecute") return "WELCOME!";
-  throw "Invalid Password";
-};
+// const login = async (username, password) => {
+//   if (!username || !password) throw "Missing Credentials";
+//   if (password === "corgifeetarecute") return "WELCOME!";
+//   throw "Invalid Password";
+// };
 
-login("todd", "corgifeetarecute")
-  .then((msg) => {
-    console.log("LOGGED IN!");
-    console.log(msg);
-  })
-  .catch((err) => {
-    console.log("ERROR!");
-    console.log(err);
-  });
+// login("todd", "corgifeetarecute")
+//   .then((msg) => {
+//     console.log("LOGGED IN!");
+//     console.log(msg);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR!");
+//     console.log(err);
+//   });
 
-const delayedColorChange = (color, delay) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      document.body.style.backgroundColor = color;
-      resolve();
-    }, delay);
-  });
-};
+// const delayedColorChange = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay);
+//   });
+// };
 
-// delayedColorChange('red', 1000)
-//     .then(() => delayedColorChange('orange', 1000))
-//     .then(() => delayedColorChange('yellow', 1000))
-//     .then(() => delayedColorChange('green', 1000))
-//     .then(() => delayedColorChange('blue', 1000))
-//     .then(() => delayedColorChange('indigo', 1000))
-//     .then(() => delayedColorChange('violet', 1000))
+// // delayedColorChange('red', 1000)
+// //     .then(() => delayedColorChange('orange', 1000))
+// //     .then(() => delayedColorChange('yellow', 1000))
+// //     .then(() => delayedColorChange('green', 1000))
+// //     .then(() => delayedColorChange('blue', 1000))
+// //     .then(() => delayedColorChange('indigo', 1000))
+// //     .then(() => delayedColorChange('violet', 1000))
 
-async function rainbow() {
-  await delayedColorChange("red", 1000);
-  await delayedColorChange("orange", 1000);
-  await delayedColorChange("yellow", 1000);
-  await delayedColorChange("green", 1000);
-  await delayedColorChange("blue", 1000);
-  await delayedColorChange("indigo", 1000);
-  await delayedColorChange("violet", 1000);
-  return "ALL DONE!";
-}
+// async function rainbow() {
+//   await delayedColorChange("red", 1000);
+//   await delayedColorChange("orange", 1000);
+//   await delayedColorChange("yellow", 1000);
+//   await delayedColorChange("green", 1000);
+//   await delayedColorChange("blue", 1000);
+//   await delayedColorChange("indigo", 1000);
+//   await delayedColorChange("violet", 1000);
+//   return "ALL DONE!";
+// }
 
-// rainbow().then(() => console.log("END OF RAINBOW!"))
+// // rainbow().then(() => console.log("END OF RAINBOW!"))
 
-async function printRainbow() {
-  await rainbow();
-  console.log("END OF RAINBOW!");
-}
+// async function printRainbow() {
+//   await rainbow();
+//   console.log("END OF RAINBOW!");
+// }
 
-printRainbow();
+// printRainbow();
 
 const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
@@ -80,7 +80,6 @@ const fakeRequest = (url) => {
     }, delay);
   });
 };
-
 async function makeTwoRequests() {
   try {
     let data1 = await fakeRequest("/page1");
