95a20b534ac1087d7e3d55c1688c81ea4dcb5a32
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-11-18 19:02:52 -0500

    changed

--- OOP_Code/07_Extends_and_Super/app.js
+++ OOP_Code/07_Extends_and_Super/app.js
@@ -1,30 +1,59 @@
 class Pet {
-	constructor(name, age) {
-		console.log('IN PET CONSTRUCTOR!');
-		this.name = name;
-		this.age = age;
-	}
-	eat() {
-		return `${this.name} is eating!`;
-	}
+  constructor(name, age) {
+    console.log("IN PET CONSTRUCTOR!");
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating!`;
+  }
 }
 
 class Cat extends Pet {
-	constructor(name, age, livesLeft = 9) {
-		console.log('IN CAT CONSTRUCTOR!');
-		super(name, age);
-		this.livesLeft = livesLeft;
-	}
-	meow() {
-		return 'MEOWWWW!!';
-	}
+  constructor(name, age, livesLeft = 9) {
+    console.log("IN CAT CONSTRUCTOR!");
+    super(name, age);
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return "MEOWWWW!!";
+  }
 }
 
 class Dog extends Pet {
-	bark() {
-		return 'WOOOF!!';
+  bark() {
+    return "WOOOF!!";
+  }
+  eat() {
+    return `${this.name} scarfs his food!`;
+  }
+}
+
+
+
+// practise
+class Pet {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
 	}
-	eat() {
-		return `${this.name} scarfs his food!`;
+	eat(){
+		return `${this.name} is eating`;
+	}
+}
+
+class Dog extends Pet{
+	constructor(name,age,lives){
+		super(name,age);
+		this.livesLeft=this.livesLeft;
+	}
+	bark(){
+		return `${this.name} is barking`;
+	}
+}
+
+class Cat extends Pet{
+	meow(){
+		return `${this.name} is meowing`;
 	}
 }
