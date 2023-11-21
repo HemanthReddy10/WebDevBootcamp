d167b41175143fd97e0a5bb14f9a71ea2d9a28ae
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-11-21 01:29:01 -0500

    REST DEMO

---
+++ RESTful_Routes_CODE/RestPractise/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const { v4: uuid } = require("uuid"); //For generating ID's
+uuid();
+const path = require("path");
+//To parse form data in POST request body:
+app.use(express.urlencoded({ extended: true }));
+// To parse incoming JSON in POST request body:
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/tacos", (req, res) => {
+  res.send("Get /tacos response");
+});
+
+app.post("/tacos", (req, res) => {
+  const { meat, qty } = req.body;
+  res.send(`here are your ${qty}${meat}tacos`);
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
+
+let comments = [
+  {
+    id: uuid(),
+    username: "Todd",
+    comment: "lol that is so funny!",
+  },
+  {
+    id: uuid(),
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    id: uuid(),
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    id: uuid(),
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index.ejs", { comments });
+});
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new", { comments });
+});
+app.post("/comments", (req, res) => {
+  console.log(req.body);
+
+  const { username, comment } = req.body;
+  comments.push({ username, comment ,id:uuid()});
+  res.redirect("/comments");
+});
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+})
