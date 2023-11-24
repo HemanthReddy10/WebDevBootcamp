ff01f47a53cc41b17bc1e49f6d8a5a65e319959f
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-11-24 01:10:39 -0500

    mongoose crud

--- MongooseBasics/MongoosePractise/index.js
+++ MongooseBasics/MongoosePractise/index.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const nodemon = require('nodemon');
+
+// movieApp---> movieSchema------>Movie----->some variable
 
 main().catch(err => console.log(err));
 
@@ -8,4 +11,42 @@ async function main() {
    
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+// it will creates a collection movies 
+
+
+// we can see this in node shell 
+// steps:
+// node
+// .load index.js
+// amadeus
+// amadeus.save()
+
+// mongosh
+// use movieApp
+// debug.movies.find()
+
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+
+
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+    .then(data => {
+        console.log("IT WORKED!")
+        console.log(data);
+    })
+
 
