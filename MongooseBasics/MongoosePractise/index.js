85ca83603eb6695936e013a3887e5d98667afbdc
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-11-23 19:00:59 -0500

    mongoose started

---
+++ MongooseBasics/MongoosePractise/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/movieApp');
+   console.log('Connection success');
+   
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
