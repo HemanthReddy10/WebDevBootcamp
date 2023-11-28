a17b925ae08f0afc435f0f493952b3f49a999882
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-11-27 23:50:42 -0500

    created views

---
+++ YelpCampProject/app.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+    // res.send("Hello from YelpCamp!");
+})
+
+app.listen(3000,()=>{
+    console.log('app listening on 3000 port');
+    
+})
