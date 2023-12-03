1b6ab7684185fee60b0c7e1bcecf5fd630b12147
HemanthReddy10 <yaramala.h@northeastern.edu>
2023-12-02 22:42:12 -0500

    errors

--- Express_Errors_CODE/Error_Basics/index.js
+++ Express_Errors_CODE/Error_Basics/index.js
@@ -1,58 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const AppError = require('./AppError');
+const AppError = require("./AppError");
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 app.use((req, res, next) => {
-    req.requestTime = Date.now();
-    console.log(req.method, req.path);
-    next();
-})
+  req.requestTime = Date.now();
+  console.log(req.method, req.path);
+  next();
+});
 
-app.use('/dogs', (req, res, next) => {
-    console.log("I LOVE DOGS!!")
-    next();
-})
+app.use("/dogs", (req, res, next) => {
+  console.log("I LOVE DOGS!!");
+  next();
+});
 
 const verifyPassword = (req, res, next) => {
-    const { password } = req.query;
-    if (password === 'chickennugget') {
-        next();
-    }
-    throw new AppError('password required', 401);
-    // res.send("PASSWORD NEEDED!")
-    // throw new AppError('Password required!', 400)
-}
+  const { password } = req.query;
+  if (password === "chickennugget") {
+    next();
+  }
+  throw new AppError("password required", 401);
+  // res.send("PASSWORD NEEDED!")
+  // throw new AppError('Password required!', 400)
+};
 
-app.get('/', (req, res) => {
-    console.log(`REQUEST DATE: ${req.requestTime}`)
-    res.send('HOME PAGE!')
-})
+app.get("/", (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`);
+  res.send("HOME PAGE!");
+});
 
-app.get('/error', (req, res) => {
-    chicken.fly()
-})
+app.get("/error", (req, res) => {
+  chicken.fly();
+});
 
-app.get('/dogs', (req, res) => {
-    console.log(`REQUEST DATE: ${req.requestTime}`)
-    res.send('WOOF WOOF!')
-})
+app.get("/dogs", (req, res) => {
+  console.log(`REQUEST DATE: ${req.requestTime}`);
+  res.send("WOOF WOOF!");
+});
 
-app.get('/secret', verifyPassword, (req, res) => {
-    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
-})
+app.get("/secret", verifyPassword, (req, res) => {
+  res.send(
+    "MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone"
+  );
+});
 
-app.get('/admin', (req, res) => {
-    throw new AppError('You are not an Admin!', 403)
-})
+app.get("/admin", (req, res) => {
+  throw new AppError("You are not an Admin!", 403);
+});
 
 app.use((req, res) => {
-    res.status(404).send('NOT FOUND!')
-})
-
+  res.status(404).send("NOT FOUND!");
+});
 
 // app.use((err, req, res, next) => {
 //     console.log("******************************************")
@@ -63,11 +64,10 @@ app.use((req, res) => {
 // })
 
 app.use((err, req, res, next) => {
-    const { status = 500, message = 'Something Went Wrong' } = err;
-    res.status(status).send(message)
-})
-
+  const { status = 500, message = "Something Went Wrong" } = err;
+  res.status(status).send(message);
+});
 
 app.listen(3000, () => {
-    console.log('App is running on localhost:3000')
-})
+  console.log("App is running on localhost:3000");
+});
